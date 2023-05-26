5c71a65a250ac26f33beeed1fc86bd225c7ea3f0
lucaspuc <95505164+lucaspuc@users.noreply.github.com>
2023-05-25 23:19:45 -0300

    ajustes leitor arquivos (#2)
    Co-authored-by: Lucas Gomes <lukasgss37@gmail.com>

---
+++ codigo/Audiencia.java
@@ -0,0 +1,13 @@
+package codigo;
+
+public class Audiencia {
+    private String login;
+    private String statusAssistido;
+    private String idSerie;
+
+    public Audiencia(String login, String statusAssistido, String idSerie) {
+        this.login = login;
+        this.statusAssistido = statusAssistido;
+        this.idSerie = idSerie;
+    }
+}
