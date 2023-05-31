0a1207d5f22ee08bb822fbd6dcfcf7d2dcbd3644
lucaspuc <95505164+lucaspuc@users.noreply.github.com>
2023-05-30 21:39:59 -0300

    leitura dos arquivos de filmes e polimorfismo entre filmes e series por meio da classe 'Conteudo' (#5)

---
+++ codigo/Conteudo.java
@@ -0,0 +1,12 @@
+package codigo;
+
+public abstract class Conteudo {
+    protected int id;
+    protected String nome;
+    protected String genero;
+    protected String idioma;
+    protected int numVisualizacoes = 0;
+    protected long mediaAvaliacoes = 0;
+    protected int numAvaliacoes = 0;
+    protected String dataLancamento;
+}
--- codigo/Interfaces/ILeitorDeArquivo.java
+++ codigo/Interfaces/ILeitorDeArquivo.java
@@ -3,11 +3,15 @@ package codigo.Interfaces;
 import java.util.Map;
 
 import codigo.Audiencia;
-import codigo.Serie;
 import codigo.Cliente;
+import codigo.Conteudo;
 
 public interface ILeitorDeArquivo {
     Map<String, Audiencia> lerArquivosAudiencia() throws Exception;
+
     Map<String, Cliente> lerArquivosEspectadores() throws Exception;
-    Map<String, Serie> lerArquivosSerie() throws Exception;
+
+    Map<String, Conteudo> lerArquivosSerie() throws Exception;
+
+    Map<String, Conteudo> lerArquivosFilme() throws Exception;
 }
